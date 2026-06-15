0e778aa4d7a8d30bd1972cb523832b791132a4b5
Fedor Romanov <fedor1378romanov@gmail.com>
2026-06-15 14:12:48 +0200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   // site: '#',
-  base: '/portfolio/',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
